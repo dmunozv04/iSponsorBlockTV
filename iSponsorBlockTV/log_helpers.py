e2075eb3f7684a9b7da33d88e0c6887cfa1a7829
rob <rob@localhost.com>
2023-10-17 19:04:58 +0100

    rm

--- iSponsorBlockTV/log_helpers.py
+++ iSponsorBlockTV/log_helpers.py
@@ -3,5 +3,5 @@ from datetime import datetime
 def info(message, device_name = None):
     device_message = ""
     if device_name:
-        device_message = f" [{device_name}]"
-    print(f"{datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')}{device_message} {message}")
+        device_message = f"[{device_name}]"
+    print(f"{device_message} {message}")
